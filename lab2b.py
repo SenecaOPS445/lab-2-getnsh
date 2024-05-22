225e74f7d02c7c9ea3a60f9027c97cca1f51f4af
Geetansh Chawla <gchawla4@myseneca.ca>
2024-05-22 18:09:21 -0400

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Prompt the user for name and age
+name = input("Name: ")
+age = input("Age: ")
+
+# Print the result
+print('Hi ' + name + ', you are ' + age + ' years old.')
