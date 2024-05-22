77f4e9c2434f4c0a4ae9866d298eeca7c9099b60
Geetansh Chawla <gchawla4@myseneca.ca>
2024-05-22 19:52:55 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author: Geetansh Chawla 
+#Author ID: 177726213
+#Date Created: 2024/05/22
+
+count = 10
+while count != 1:
+    print(count)
+    count = count - 1
+print(count)
+print("blast off!")
+ 
