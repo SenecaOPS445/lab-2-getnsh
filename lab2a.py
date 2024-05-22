f3437e678ccd14ca9e0d1f145a569b2eae3cf99d
Geetansh Chawla <gchawla4@myseneca.ca>
2024-05-22 17:57:30 -0400

    git commit -m "Added lab2a " git push

--- lab2a.py
+++ lab2a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-name = 'jon'
+name = 'Jon'
 age = 20
-print('Hi ' + name + ', you are '+ str(age) +' years old, ')
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
 
