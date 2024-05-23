5c00c644b624d34f47e2b528e8fcc3b53e5852ac
Geetansh Chawla <gchawla4@myseneca.ca>
2024-05-22 20:04:45 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Author: Geetansh Chawla 
+#Author ID: 177726213
+#Date Created: 2024/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+    
+print("blast off!")
+sys.exit()
+ 
